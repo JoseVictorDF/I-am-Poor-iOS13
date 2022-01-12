09683c1c672351d093cd4f8480a5f9d8fc9b29fe
José Victor D. Fernandes <josevictordf@gmail.com>
2022-01-12 09:39:27 -0300

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Jose Victor Fernandes on 12/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
